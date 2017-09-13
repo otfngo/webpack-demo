c62ae537c66c88aa5026a3802807e4485524e13a
otfngo <otfngo@qq.com>
2017-09-13 23:55:13 +0800

    ...

--- demo3/src/app.js
+++ demo3/src/app.js
@@ -1,7 +1,10 @@
-import layer from './components/layer/layer'
+import './common/css/index.css'
+import Layer from './components/layer/layer'
 
 const App = function () {
-    console.log(layer)
+    let dom = document.getElementById('app')
+    let layer = new Layer()
+    dom.innerHTML = layer.tpl
 }
 
 new App()
--- demo3/webpack.config.js
+++ demo3/webpack.config.js
@@ -1,23 +1,76 @@
 const path = require('path')
-let htmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name]-[chunkhash].js'
+        filename: 'js/[name].js'
     },
     module: {
         rules: [
             {
                 test: /\.jsx?$/,
-                exclude: [
-                  path.resolve(__dirname, 'node_modules')
+                include: [
+                    path.resolve(__dirname, 'src')
                 ],
-                loader: 'babel-loader'
+                exclude: [
+                    path.resolve(__dirname, 'node_modules')
+                ],
+                loader: 'babel-loader',
+                options: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: (loader) => [
+                                    require('postcss-import')(),
+                                    require('autoprefixer')({
+                                        browsers: ['last 5 versions']
+                                    })
+                                ]
+                            }
+                        }
+                    ]
+                })
+            },
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: (loader) => [
+                                    require('postcss-import')(),
+                                    require('autoprefixer')({
+                                        browsers: ['last 5 versions']
+                                    })
+                                ]
+                            }
+                        },
+                        'less-loader'
+                    ]
+                })
             }
         ]
-    }
+    },
     plugins: [
         new htmlWebpackPlugin({
             filename: 'index.html',
@@ -28,6 +81,20 @@ module.exports = {
                 collapseWhitespace: false,
                 keepClosingSlash: true
             }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                drop_console: false
+            }
+        }),
+        new ExtractTextPlugin({
+            filename: 'css/[name].css',
+            allChunks: true
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: 'js/vendor.min.js'
         })
     ]
 }
